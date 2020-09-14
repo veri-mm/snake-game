b9d852659917e37cc1cfd63f054708945b992e6a
Veridiana Máximo <69319528+veri-mm@users.noreply.github.com>
2020-09-14 10:13:06 -0300

    define snake movement

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
-function createBG() {
+function createBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
@@ -19,5 +20,28 @@ function createSnake(){
     }
 }
 
-createBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake();
+
+    let snakeY = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+    
+}
+
+let game = setInterval(startGame, 100);
+
