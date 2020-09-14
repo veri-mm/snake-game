354850d9778a3e67e06f2863ee22b8c47d8a0b47
Veridiana Máximo <69319528+veri-mm@users.noreply.github.com>
2020-09-14 11:45:20 -0300

    define snake length

--- script.js
+++ script.js
@@ -41,8 +41,15 @@ function update (event){
 function startGame(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction =="down") snake[0].y = 0;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i= 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert('Game Over');
+        }
+    }
 
     createBG();
     createSnake();
@@ -56,7 +63,12 @@ function startGame(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
